7acd6d5ff0f2160285b4a87b762bcf077511a8ac
nruizu <140680196+nruizu@users.noreply.github.com>
2026-02-24 20:52:35 -0500

    Limpiar settings

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-#Hola
+
 from pathlib import Path
 import environ
 import os
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
