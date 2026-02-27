7aa4234871a0eb6f787045d5f1437d5ce44f352f
Alejandro Hinestroza <ahinestrog@eafit.edu.co>
2026-02-27 07:59:57 -0500

    Reestructuración directorios y dockerización front

--- backend/manage.py
+++ backend/manage.py
@@ -3,6 +3,14 @@
 
 import os
 import sys
+from pathlib import Path
+
+
+CURRENT_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = CURRENT_DIR.parent
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
 
 
 def main():
