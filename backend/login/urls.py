45e7c046abfce8abfcb73f4df9ce7f2129455889
Alejandro Hinestroza <124910059+ahinestrog@users.noreply.github.com>
2026-03-17 06:42:56 -0500

    Revert "Feature/hu4 hu5 pedidos programados"

--- backend/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import login_view, me_view, register_view, logout_view
-
-urlpatterns = [
-    path("login/", login_view, name="api_login"),
-    path("registro/", register_view, name="api_register"),
-    path("logout/", logout_view, name="api_logout"),
-    path("me/", me_view, name="api_me"),
-]
