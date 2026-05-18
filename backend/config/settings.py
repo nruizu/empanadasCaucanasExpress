bb57854467e02eadaff29d5a1b879c6425e3d485
nicolas <nruizu@eafit.edu.co>
2026-05-18 10:57:43 -0500

    Cambio despliegue Backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,13 +89,9 @@ WSGI_APPLICATION = "backend.config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
